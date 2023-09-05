13ce3a61ab6365c24c895b871738031898fb8601
Jean Carlos Ruiz <66891780+jeancarlosruiz@users.noreply.github.com>
2023-09-04 22:11:21 -0400

    width and height accessibility fixed

--- src/components/Benefits/Benefits.jsx
+++ src/components/Benefits/Benefits.jsx
@@ -3,24 +3,32 @@ import style from './Benefits.module.css'
 const benefitsCard = [
 	{
 		img: 'images/illustration-passions.svg',
+		width:88,
+		height:106,
 		title: 'Indulge your passions',
 		description:
 			"Your passions shouldn't be just for the weekend. Earn a living doing what you love.",
 	},
 	{
 		img: 'images/illustration-financial-freedom.svg',
+		width:74,
+		height:89,
 		title: 'Gain financial freedom',
 		description:
 			'Start making money work for you. There’s nothing quite like earning while you sleep. ',
 	},
 	{
 		img: 'images/illustration-lifestyle.svg',
+		width:80,
+		height:94,
 		title: 'Choose your lifestyle',
 		description:
 			'Own your daily schedule. Fancy a lie-in? Go for it! Take charge of your week.',
 	},
 	{
 		img: 'images/illustration-work-anywhere.svg',
+		width:104,
+		height:74,
 		title: 'Work from anywhere',
 		description:
 			'Selling online means not being pinned down. Want to work AND travel? Go for it!',
@@ -36,6 +44,8 @@ function Benefits() {
 					<div className={style.card} key={i}>
 						<div className={style['img-container']}>
 							<img
+								width={benefit.width}
+								height={benefit.height}
 								className={style.img}
 								src={benefit.img}
 								alt="image benefit"
